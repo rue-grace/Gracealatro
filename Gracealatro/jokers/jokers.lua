46d15212ffa704e3416c220a914e8b10728d44da
Rue <ruegreacefan10@gmail.com>
2025-10-21 19:59:44 -0300

    fixed atlas mistake
    fixed an atlas mistake that made the jokers unobservable (made the game crash)

--- Gracealatro/jokers/jokers.lua
+++ Gracealatro/jokers/jokers.lua
@@ -86,6 +86,34 @@ SMODS.Atlas({
     py = 95
 })
 
+SMODS.Atlas({
+    key = "heed",
+    path = "j_heed.png",
+    px = 71,
+    py = 95
+})
+
+SMODS.Atlas({
+    key = "elkman",
+    path = "j_elkman.png",
+    px = 71,
+    py = 95
+})
+
+SMODS.Atlas({
+    key = "slugfish",
+    path = "j_slugfish.png",
+    px = 35.5,
+    py = 47.5
+})
+
+SMODS.Atlas({
+    key = "slight",
+    path = "j_slight.png",
+    px = 71,
+    py = 95
+})
+
 SMODS.Joker{
     key = "john_zen",
     config = { extra = { chips = 750, mult = 15 } },
@@ -391,6 +419,298 @@ SMODS.Joker{
             }
         end
     end
+}
+
+SMODS.Joker{ --Heed
+    key = "heed",
+    config = {
+        extra = {
+            multvar = 2,
+            probabilityvar = 5,
+            odds = "card.ability.extra.probabilityvar",
+            odds2 = 2,
+            start_dissolve = 0,
+            n = 0
+        }
+    },
+    pos = { x = 0, y = 0 },
+    cost = 4,
+    rarity = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'heed',
+
+    loc_vars = function(self, info_queue, card)
+        
+        local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'j_sim_heed')
+        local new_numerator2, new_denominator2 = SMODS.get_probability_vars(card, 1, card.ability.extra.odds2, 'j_sim_heed')
+        return {vars = {card.ability.extra.multvar, card.ability.extra.probabilityvar, new_numerator, new_denominator, new_numerator2, new_denominator2}}
+    end,
+
+    
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play  then
+            if true then
+                return {
+                    mult = card.ability.extra.multvar
+                    ,
+                    func = function()
+                        if SMODS.pseudorandom_probability(card, 'group_0_efdf165c', 1, undefined, 'j_sim_heed', false) then
+                                SMODS.calculate_effect({func = function()
+                                    card:start_dissolve()
+                                    return true
+                                    end}, card)
+                                end
+                                return true
+                                end
+                            }
+                        else
+                            card.ability.extra.multvar = (card.ability.extra.multvar) + 5
+                        end
+                    end
+                    if context.setting_blind  then
+                        if true then
+                            if SMODS.pseudorandom_probability(card, 'group_0_3fcd3b90', 1, card.ability.extra.odds2, 'j_sim_heed', false) then
+                                    SMODS.calculate_effect({func = function()
+                                        card.ability.extra.probabilityvar = (card.ability.extra.probabilityvar) + 2
+                                        return true
+                                        end}, card)
+                                    end
+                                    if SMODS.pseudorandom_probability(card, 'group_1_d6b7c18a', 1, card.ability.extra.odds2, 'j_sim_heed', false) then
+                                            SMODS.calculate_effect({func = function()
+                                                card.ability.extra.probabilityvar = math.max(0, (card.ability.extra.probabilityvar) - 2)
+                                                return true
+                                                end}, card)
+                                            end
+                                        end
+                                    end
+                                end
+}
+
+SMODS.Joker{ --Elkman
+    key = "elkman",
+    config = {
+        extra = {
+            edition = 0,
+            odds = 4,
+            start_dissolve = 0,
+            n = 0,
+            respect = 0
+        }
+    },
+    pos = { x = 0, y = 0 },
+    cost = 4,
+    rarity = 3,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'elkman',
+
+    
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+            if true then
+                return {
+                    func = function()
+                        card:start_dissolve()
+                        return true
+                        end
+                        ,
+                        func = function()
+                            if SMODS.pseudorandom_probability(card, 'group_0_fe82f0d7', 1, card.ability.extra.odds, 'j_sim_elkman', false) then
+                                local created_joker = false
+                                if #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit then
+                                    created_joker = true
+                                    G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+                                    G.E_MANAGER:add_event(Event({
+                                    func = function()
+                                        local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_sim_elkman' })
+                                        if joker_card then
+                                            joker_card:set_edition("e_foil", true)
+                                            
+                                        end
+                                        G.GAME.joker_buffer = 0
+                                        return true
+                                        end
+                                    }))
+                                end
+                                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = created_joker and localize('k_plus_joker') or nil, colour = G.C.BLUE})
+                            end
+                            if SMODS.pseudorandom_probability(card, 'group_1_74fd3400', 1, card.ability.extra.odds, 'j_sim_elkman', false) then
+                                local created_joker = false
+                                if #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit then
+                                    created_joker = true
+                                    G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+                                    G.E_MANAGER:add_event(Event({
+                                    func = function()
+                                        local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_sim_elkman' })
+                                        if joker_card then
+                                            joker_card:set_edition("e_holo", true)
+                                            
+                                        end
+                                        G.GAME.joker_buffer = 0
+                                        return true
+                                        end
+                                    }))
+                                end
+                                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = created_joker and localize('k_plus_joker') or nil, colour = G.C.BLUE})
+                            end
+                            if SMODS.pseudorandom_probability(card, 'group_2_4e93bf4b', 1, card.ability.extra.odds, 'j_sim_elkman', false) then
+                                local created_joker = false
+                                if #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit then
+                                    created_joker = true
+                                    G.GAME.joker_buffer = G.GAME.joker_buffer + 1
+                                    G.E_MANAGER:add_event(Event({
+                                    func = function()
+                                        local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_sim_elkman' })
+                                        if joker_card then
+                                            joker_card:set_edition("e_polychrome", true)
+                                            
+                                        end
+                                        G.GAME.joker_buffer = 0
+                                        return true
+                                        end
+                                    }))
+                                end
+                                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = created_joker and localize('k_plus_joker') or nil, colour = G.C.BLUE})
+                            end
+                            if SMODS.pseudorandom_probability(card, 'group_3_8fac5b15', 1, card.ability.extra.odds, 'j_sim_elkman', false) then
+                                local created_joker = true
+                                G.E_MANAGER:add_event(Event({
+                                func = function()
+                                    local joker_card = SMODS.add_card({ set = 'Joker', key = 'j_sim_elkman' })
+                                    if joker_card then
+                                        joker_card:set_edition("e_negative", true)
+                                        
+                                    end
+                                    
+                                    return true
+                                    end
+                                }))
+                                card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = created_joker and localize('k_plus_joker') or nil, colour = G.C.BLUE})
+                            end
+                            return true
+                            end
+                        }
+                    end
+                end
+            end
+}
+
+SMODS.Joker{ --Slugfish
+    key = "slugfish",
+    config = {
+        extra = {
+            hand_change = 1,
+            emult = 4,
+            dollars = 5
+        }
+    },
+    pos = { x = 0, y = 0 },
+    cost = 4,
+    rarity = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'slugfish',
+
+    
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+            return {
+                e_mult = card.ability.extra.emult,
+                extra = {
+                dollars = -card.ability.extra.dollars,
+                colour = G.C.MONEY
+            }
+        }
+    end
+end,
+
+    add_to_deck = function(self, card, from_debuff)
+        G.GAME.round_resets.hands = math.max(1, G.GAME.round_resets.hands - card.ability.extra.hand_change)
+    end,
+
+    remove_from_deck = function(self, card, from_debuff)
+        G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.hand_change
+    end
+}
+
+SMODS.Joker{ --Slight
+    key = "slight",
+    config = {
+        extra = {
+            multvar = 2,
+            probabilityvar = 5,
+            chipsvar = 10,
+            odds = "card.ability.extra.probabilityvar",
+            odds2 = 2,
+            start_dissolve = 0,
+            n = 0
+        }
+    },
+    pos = { x = 0, y = 0 },
+    cost = 4,
+    rarity = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'slight',
+
+    loc_vars = function(self, info_queue, card)
+        
+        local new_numerator, new_denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'j_sim_slight')
+        local new_numerator2, new_denominator2 = SMODS.get_probability_vars(card, 1, card.ability.extra.odds2, 'j_sim_slight')
+        return {vars = {card.ability.extra.multvar, card.ability.extra.probabilityvar, card.ability.extra.chipsvar, new_numerator, new_denominator, new_numerator2, new_denominator2}}
+    end,
+
+    
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play  then
+            if true then
+                return {
+                    chips = card.ability.extra.chipsvar
+                    ,
+                    func = function()
+                        if SMODS.pseudorandom_probability(card, 'group_0_efdf165c', 1, undefined, 'j_sim_slight', false) then
+                                SMODS.calculate_effect({func = function()
+                                    card:start_dissolve()
+                                    return true
+                                    end}, card)
+                                end
+                                return true
+                                end
+                            }
+                        else
+                            card.ability.extra.chipsvar = (card.ability.extra.chipsvar) + 10
+                        end
+                    end
+                    if context.setting_blind  then
+                        if true then
+                            if SMODS.pseudorandom_probability(card, 'group_0_3fcd3b90', 1, card.ability.extra.odds2, 'j_sim_slight', false) then
+                                    SMODS.calculate_effect({func = function()
+                                        card.ability.extra.probabilityvar = (card.ability.extra.probabilityvar) + 2
+                                        return true
+                                        end}, card)
+                                    end
+                                    if SMODS.pseudorandom_probability(card, 'group_1_d6b7c18a', 1, card.ability.extra.odds2, 'j_sim_slight', false) then
+                                            SMODS.calculate_effect({func = function()
+                                                card.ability.extra.probabilityvar = math.max(0, (card.ability.extra.probabilityvar) - 2)
+                                                return true
+                                                end}, card)
+                                            end
+                                        end
+                                    end
+                                end
 }
 
 -- SMODS.Joker{
